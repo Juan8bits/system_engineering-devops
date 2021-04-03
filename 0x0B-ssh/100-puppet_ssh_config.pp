7b852b6061dbebe70961f0fcfa9ec75972ab81a3
Juan8bits <juan160820@gmail.com>
2021-04-03 22:36:36 +0000

    Try #4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,14 @@
 # PPFile that config ssh_config file.
-file_line { 'Use private key':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/holberton',
+file_line {'ssh_config pass':
+  ensure =>  'present',
+  path   =>  '/etc/ssh/copy_ssh_config',
+  line   =>  '    PasswordAuthentication no',
+  match  =>  '^#\s\s\sPass',
 }
 
-file_line { 'Refuse password':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
+file_line {'ssh_config identity':
+  ensure =>  'present',
+  path   =>  '/etc/ssh/copy_ssh_config',
+  line   =>  '    IdentityFile ~/.ssh/holberton',
+  match  =>  '^#\s\s\sIdentity',
 }
