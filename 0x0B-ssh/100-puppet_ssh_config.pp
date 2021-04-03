08894cb89c90aa41e9c848fc94fc47731e100b43
Juan8bits <juan160820@gmail.com>
2021-04-03 22:40:51 +0000

    Remove match, try#6

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,12 @@
 # PPFile that config ssh_config file.
-file_line {'ssh_config pass':
+file_line {'Turn off passwd auth':
   ensure =>  'present',
-  path   =>  '/etc/ssh/copy_ssh_config',
+  path   =>  '/etc/ssh/ssh_config',
   line   =>  '    PasswordAuthentication no',
-  match  =>  '^#\s\s\sPass',
 }
 
-file_line {'ssh_config identity':
+file_line {'Declare identity file':
   ensure =>  'present',
-  path   =>  '/etc/ssh/copy_ssh_config',
+  path   =>  '/etc/ssh/ssh_config',
   line   =>  '    IdentityFile ~/.ssh/holberton',
-  match  =>  '^#\s\s\sIdentity',
 }
