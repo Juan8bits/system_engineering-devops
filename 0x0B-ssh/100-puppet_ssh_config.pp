a6b98209109b0e45a493faa1c85ea4a921292449
Juan8bits <juan160820@gmail.com>
2021-04-03 22:35:34 +0000

    Try #3

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,10 @@
 # PPFile that config ssh_config file.
-file_line{ 'Turn off passwd auth':
-    ensure => 'present',
-    path   => '/etc/ssh/ssh_config',
-    line   => '    PasswordAuthentication no',
-    match  => '(PasswordAuthentication.no)\n',
+file_line { 'Use private key':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
 }
 
-file_line{ 'Declare identity file':
-    ensure => 'present'
-    path   => '/etc/ssh/ssh_config',
-    line   => '    IdentityFile ~/.ssh/holberton',
-    match  => '(IdentityFile.+)\n',
+file_line { 'Refuse password':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
 }
