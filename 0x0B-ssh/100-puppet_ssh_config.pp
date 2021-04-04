f5a5568fd312351ff196a2d62ffb97e044ac7e60
Juan Manuel Ramírez S <70917491+Juan8bits@users.noreply.github.com>
2021-04-04 00:29:42 -0500

    Include stdlib and remove ensure

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # PPFile that config ssh_config file.
+include stdlib
+
 file_line {'Turn off passwd auth':
-  ensure =>  'present',
   path   =>  '/etc/ssh/ssh_config',
-  line   =>  '    PasswordAuthentication no',
+  line   =>  'PasswordAuthentication no',
 }
 
 file_line {'Declare identity file':
-  ensure =>  'present',
   path   =>  '/etc/ssh/ssh_config',
-  line   =>  '    IdentityFile ~/.ssh/holberton',
+  line   =>  'IdentityFile ~/.ssh/holberton',
 }
