4e87cf622fb29eaa1614550fb132a5559844bd48
Juan8bits <juan160820@gmail.com>
2021-04-04 17:55:03 +0000

    Fix puppet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # PPFile-> Create a file in /tmp in which:
-#   File path is "/tmp/holberton"
-#   File permission is "0744"
-#   File owner is "www-data"
-#   File group is "www-data"
-#   File contains "I love Puppet"
+#   File path is "/tmp/holberton".
+#   File permission is "0744".
+#   File owner is "www-data".
+#   File group is "www-data".
+#   File contains "I love Puppet".
 file {'holberton':
     ensure  => 'present',
     path    => '/tmp/holberton',
