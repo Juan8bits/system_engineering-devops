d3c968f0145e9817b941a14690104fc2ea998891
Juan8bits <juan160820@gmail.com>
2021-04-04 18:01:26 +0000

    The checker is broken :broken_heart:

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# PPFile that config ssh_config file.
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  path =>  '/etc/ssh/ssh_config',
+  line =>  'PasswordAuthentication no',
+}
+
+file_line {  'Declare identity file':
+  path =>  '/etc/ssh/ssh_config',
+  line =>  'IdentityFile ~/.ssh/holberton',
+}
